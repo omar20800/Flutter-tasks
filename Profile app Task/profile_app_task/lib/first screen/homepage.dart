fbf956b6f0b9e061f832afd5cc8bcb31b507495a
Omar Elkhadrawy <lodamora20800@gmail.com>
2025-02-02 00:06:19 +0200

    scrolling issue fixed

--- Profile app Task/profile_app_task/lib/first screen/homepage.dart
+++ Profile app Task/profile_app_task/lib/first screen/homepage.dart
@@ -21,131 +21,118 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20) ,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  ClipOval(
-                    child: 
-                    Image.asset("assets/images/71665268.jpg",width: 100.0,)
-                    ),
-                    SizedBox(width: 20,),
-                  Column(
-                    children: const [
-                      Text("Sayed Abdul-Aziz",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                      Text("Sayed@gmail.com",style: TextStyle(fontSize: 15,color: Colors.grey),),
-                    ],
-                    ),
-                ],
-              ),
-            ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ClipOval(
+                      child: 
+                      Image.asset("assets/images/71665268.jpg",width: 100.0,)
+                      ),
+                      SizedBox(width: 20,),
+                    Column(
                       children: const [
-                        Text("My Orders",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        Text("Already have 10 orders",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        Text("Sayed Abdul-Aziz",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                        Text("Sayed@gmail.com",style: TextStyle(fontSize: 15,color: Colors.grey),),
                       ],
                       ),
-                  SizedBox(width: 150,),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios),
-                  )
-                ],
-              ),
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("My Orders",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("Already have 10 orders",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        ],
+                        ),
+                    Expanded(child: Container(height: 10,)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Shipping Adresses",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("3 Adresses",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        ],
+                        ),
+                    Expanded(child: Container(height: 10,)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Payment Method",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("you have 2 cards",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        ],
+                        ),
+                    Expanded(child: Container(height: 10,)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("My Reviews",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("reviews for 5 items",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        ],
+                        ),
+                    Expanded(child: Container(height: 10,)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Settings",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("Notification, Password, FAQ, Contact",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                        ],
+                        ),
+                    Expanded(child: Container(height: 10,)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_forward_ios),
+                    )
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Shipping Adresses",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        Text("3 Adresses",style: TextStyle(fontSize: 15,color: Colors.grey),),
-                      ],
-                      ),
-                  SizedBox(width: 135,),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Payment Method",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        Text("you have 2 cards",style: TextStyle(fontSize: 15,color: Colors.grey),),
-                      ],
-                      ),
-                  SizedBox(width: 150,),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("My Reviews",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        Text("reviews for 5 items",style: TextStyle(fontSize: 15,color: Colors.grey),),
-                      ],
-                      ),
-                  SizedBox(width: 175,),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Settings",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                        Text("Notification, Password, FAQ, Contact",style: TextStyle(fontSize: 15,color: Colors.grey),),
-                      ],
-                      ),
-                  SizedBox(width: 54,),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_forward_ios),
-                  )
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
