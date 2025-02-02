810da2c61370c389e571e380c0ebafb4d66d6cc7
Omar Elkhadrawy <lodamora20800@gmail.com>
2025-02-02 05:25:58 +0200

    refactor: update ignore_for_file comments for clarity

--- Resturant app Task/resturant_app_task/lib/Home/HomePage.dart
+++ Resturant app Task/resturant_app_task/lib/Home/HomePage.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'dart:math';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names
 
 import 'package:flutter/material.dart';
 
